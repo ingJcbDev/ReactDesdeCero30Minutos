d2acd04faa71fa8a0768f05b7ab6adba0194f709
ingJcbDev <ing.jcb.dev@gmail.com>
2021-08-02 13:36:11 -0500

    Commit inicial

---
+++ react-todos/src/index.js
@@ -0,0 +1,7 @@
+//importar libreria para escribir jsx es javaScript para escribir html
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
